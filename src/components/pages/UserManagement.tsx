b75fcb1d9a304c885a8f3b7b4e3d592f06e3ee14
kotaro <kotaroxx0909xx@gmail.com>
2021-12-23 23:08:46 +0900

    ユーザー一覧の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,15 +1,32 @@
-import { Wrap, WrapItem } from '@chakra-ui/react';
-import { memo, VFC } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
+import { memo, useEffect, VFC } from 'react';
 import { UserCard } from '../organisms/layout/user/UserCard';
+import { useAllUsers } from '../../hooks/useAllUsers';
+import { User } from '../../types/user';
 
-export const UserManagement: VFC = memo(() => (
-  <Wrap p={{ base: 4, md: 10 }}>
-    <WrapItem>
-      <UserCard
-        imageUrl="https://source.unsplash.com/ramdom"
-        userName="じゃけぇ"
-        fullName="Takumi Okada"
-      />
-    </WrapItem>
-  </Wrap>
-));
+export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+  useEffect(() => getUsers(), []);
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {users?.map((user: User) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/ramdom"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
+});
